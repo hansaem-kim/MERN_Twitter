cfcd150178dfcaacd352bb4adfb09fbbb2c8954c
Hansaem Kim <hansaem_kim@hotmail.com>
2021-09-18 22:48:32 -0400

    Ready to deploy

--- app.js
+++ app.js
@@ -21,30 +21,14 @@ mongoose
     .then(() => console.log("Connected to mongoDB"))
     .catch(err => console.log(err));
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-
-
 app.use(passport.initialize());
-require('./confg/passport')(passport);
+require('./confg/passport')(passport);    
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-
-app.get("/", (req, res) => {
-    const user = new User({
-        handle: "hansaem",
-        email: "hansaem@hansaem.com",
-        password: "hansaem"
-    })
-    user.save();
-    res.send("Hello World!");
-});
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
-
-app.listen(port, () => {console.log(`Listening on port ${port}`)});
+app.listen(port, () => console.log(`Server is running on port ${port}`));
