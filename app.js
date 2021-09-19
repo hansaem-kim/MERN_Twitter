200dbd2e397497e47e7990ca1582bbf23988b084
Hansaem Kim <hansaem_kim@hotmail.com>
2021-09-18 21:03:45 -0400

    Setup React

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./confg/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require("./models/User");
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to mongoDB"))
+    .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+
+app.use(passport.initialize());
+require('./confg/passport')(passport);
+
+app.use(bodyParser.json());
+
 
 app.get("/", (req, res) => {
+    const user = new User({
+        handle: "hansaem",
+        email: "hansaem@hansaem.com",
+        password: "hansaem"
+    })
+    user.save();
     res.send("Hello World!");
 });
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
